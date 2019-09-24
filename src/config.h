618db309fabfc14f92c150d70194ccea40ca216c
Aitor Moreno <aitor@rojo2.com>
2019-09-25 01:49:51 +0200

    Change name to WebDOOM

--- src/config.h
+++ src/config.h
@@ -6,25 +6,25 @@
 #define HAVE_LIBPNG
 
 /* Name of package */
-#define PACKAGE "wasm-doom"
+#define PACKAGE "web-doom"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "lazarv1982@gmail.com"
+#define PACKAGE_BUGREPORT "hola@rojo2.com"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "WebAssembly DOOM"
+#define PACKAGE_NAME "WebDOOM"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "WebAssembly DOOM 1.0.0"
+#define PACKAGE_STRING "WebDOOM 1.0.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "wasm-doom"
+#define PACKAGE_TARNAME "web-doom"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.0"
+#define PACKAGE_VERSION "1.0.1"
 
 /* Change this when you create your awesome forked version */
-#define PROGRAM_PREFIX "wasm-doom-"
+#define PROGRAM_PREFIX "web-doom-"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
